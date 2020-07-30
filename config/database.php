e80ee3276a56c73df6a02f0f080b20ed582103c1
my_tech_147 <usman.arif9219@gmail.com>
2020-07-30 18:00:04 +0500

    config database updated

--- config/database.php
+++ config/database.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL=parse_url('DATABASE_URL');
 return [
 
     /*
@@ -15,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,17 +67,32 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => $DATABASE_URL["path"],
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
+
+//        'pgsql' => [
+//            'driver' => 'pgsql',
+//            'url' => env('DATABASE_URL'),
+//            'host' => isset($DATABASE_URL["host"]) ? $DATABASE_URL["host"] : null,
+//            'port' => isset($DATABASE_URL["port"]) ? $DATABASE_URL["port"] : null,
+//            'database' => isset($DATABASE_URL["path"]) ? ltrim($DATABASE_URL["path"],"/") : null,
+//            'username' => isset($DATABASE_URL["user"]) ? $DATABASE_URL["user"] : null,
+//            'password' => isset($DATABASE_URL["pass"]) ? $DATABASE_URL["pass"] : null,
+//            'charset' => 'utf8',
+//            'prefix' => '',
+//            'prefix_indexes' => true,
+//            'schema' => 'public',
+//            'sslmode' => 'prefer',
+//        ],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
