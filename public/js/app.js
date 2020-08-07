3674936daf98b5522c7816d2af0362c7e29dc116
my_tech_147 <usman.arif9219@gmail.com>
2020-08-07 16:57:02 +0500

    js precompilation

--- public/js/app.js
+++ public/js/app.js
@@ -98267,8 +98267,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\laravel 5.8\Teacher_Portal_System\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\laravel 5.8\Teacher_Portal_System\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\laravel 5.8\final year project\Teacher_Portal_System\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\laravel 5.8\final year project\Teacher_Portal_System\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
